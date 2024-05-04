8e2c3fca5fa8e1222ea289aea2239f0a56e306fc
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-04 12:29:13 +0200

    exercise completed

--- index.php
+++ index.php
@@ -61,19 +61,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>hotels</h1>
-    <ul>
-        <?php foreach($hotels as $hotel) { ?>
-            <li>
-                <div> nome: <?php echo $hotel["name"] ?></div>
-                <div> descrizione: <?php echo $hotel["description"] ?></div>
-                <div> parcheggio: <?php echo $hotel["parking"] ?></div>
-                <div> voto: <?php echo $hotel["vote"] ?></div>
-                <div> distanza dal centro: <?php echo $hotel["distance_to_center"] ?></div>
-            </li>
-        <?php } ?>
-    </ul>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) { ?>
+        <tr>
+            <td><?php echo $hotel["name"] ?></td>
+            <td><?php echo $hotel["description"] ?></td>
+            <td><?php echo $hotel["parking"] ?></td>
+            <td><?php echo $hotel["vote"] ?></td>
+            <td><?php echo $hotel["distance_to_center"] ?></td>
+        </tr>
+    <?php } ?>
+  </tbody>
+</table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
